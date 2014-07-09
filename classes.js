55477dac6a0fe620da974183a3b02fa4a83ea1f6
Alex Grant <grantovich@users.noreply.github.com>
2014-07-09 18:01:54 -0400

    First version of assessment

---
+++ classes.js
@@ -0,0 +1,19 @@
+///// Question 1
+// Define a class called `Cat`, with an initializer that accepts a `name`,
+// an `age`, and a `color`, and stores these values as properties.
+
+///// Question 2
+// Add a function called `description` to the `Cat` class that returns a string
+// describing the cat in this format: "<name> is a <age>-year-old <color> cat."
+
+///// Question 3
+// Create a new instance of the `Cat` class (make up an age, name, and color)
+// and assign it to a variable.
+
+///// Question 4
+// Call the `description` function on the `Cat` instance you created in the
+// previous question, and store the result in another variable.
+
+///// Question 5
+// Set the age of the `Cat` instance you created in question 3 to one higher
+// than its current age.
