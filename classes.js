99c700ca3974b19ee8087bcc0c6915c5dd1e4b79
Jake Austin <jakeaust07@gmail.com>
2014-07-17 09:49:13 -0400

    fix description function

--- classes.js
+++ classes.js
@@ -13,7 +13,7 @@ var Cat = function(name, age, color) {
 // Add a function called `description` to the `Cat` class that returns a string
 // describing the cat in this format: "<name> is a <age>-year-old <color> cat."
 Cat.prototype.description = function() {
-  return this.name + ' is a ' + age + ' year old ' + color + ' cat.';
+  return this.name + ' is a ' + this.age + ' year old ' + this.color + ' cat.';
 };
 
 
