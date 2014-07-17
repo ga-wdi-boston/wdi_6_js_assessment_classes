cc96d8645f515b680e6a04f4262c5a4b7115d5aa
sarahchristine913 <s.mele13@gmail.com>
2014-07-17 09:55:20 -0400

    Mele_S Completed Assessment
    + Straightforward and clear! Feel good about it.

--- classes.js
+++ classes.js
@@ -2,18 +2,38 @@
 // Define a class called `Cat`, with an initializer that accepts a `name`,
 // an `age`, and a `color`, and stores these values as properties.
 
+var Cat = function(name, age, color){
+  this.name =  name;
+  this.age = age;
+  this.color = color;
+};
+
 ///// Question 2
 // Add a function called `description` to the `Cat` class that returns a string
 // describing the cat in this format: "<name> is a <age>-year-old <color> cat."
 
+Cat.prototype = {
+  description: function(){
+    return this.name + ' is a ' + this.age + ' yr old ' + this.color + ' cat.';
+  }
+};
 ///// Question 3
 // Create a new instance of the `Cat` class (make up an age, name, and color)
 // and assign it to a variable.
+
+var louie = new Cat('Louie', '5', 'orange');
 
 ///// Question 4
 // Call the `description` function on the `Cat` instance you created in the
 // previous question, and store the result in another variable.
 
+var louieDescription = louie.description();
+
 ///// Question 5
 // Set the age of the `Cat` instance you created in question 3 to one higher
 // than its current age.
+
+louie.age = louie.age + 1;
+
+// To update the louieDescription variable:
+louieDescription = louie.description();
