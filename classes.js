b1aeb4cb4c4ed038f8cd0cd8513f88defb9d17b4
Jodi Detch <jodidetch@gmail.com>
2014-07-17 10:24:01 -0400

    Assessment updated with correct answers/comments

--- classes.js
+++ classes.js
@@ -8,15 +8,33 @@ var Cat = function(name, age, color){
   this.color = color;
 };
 
+// OR
+
+function Cat(name, age, color){
+  this.name = name;
+  this.age = age;
+  this.color = color;
+};
+
+
 ///// Question 2
 // Add a function called `description` to the `Cat` class that returns a string
 // describing the cat in this format: "<name> is a <age>-year-old <color> cat."
 
+// Below, you are overwriting entire protype of function with new object
+// Typically preferred because you usually define all your functions in one shot
 Cat.prototype = {
   description: function(){
     return this.name + " is a" + this.age + "-year-old " + this.color + " cat.";
   }
 };
+
+// OR
+
+// Below, you are just adding a new property onto an existing prototype
+Cat.prototype.description = function(){
+   return this.name + " is a" + this.age + "-year-old " + this.color + " cat.";
+  };
 
 ///// Question 3
 // Create a new instance of the `Cat` class (make up an age, name, and color)
@@ -34,4 +52,9 @@ var newCat2 = newCat.description();
 // Set the age of the `Cat` instance you created in question 3 to one higher
 // than its current age.
 
+// MY ANSWER
 newCat.age += age++;
+
+// CORRECT
+
+newCat.age += 1;
